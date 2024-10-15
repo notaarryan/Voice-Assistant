bc864bd39a49fe2b84648463b0c0593c48d753a8
notaarryan <aryanparmar2503@gmail.com>
2024-10-15 02:23:09 -0400

    imported the req modules and worked on speech recognition

---
+++ main.py
@@ -0,0 +1,29 @@
+import speech_recognition as sr
+import webbrowser
+import pyttsx3
+
+engine = pyttsx3.init()
+engine.setProperty('rate',150)
+
+def speak(text :str):
+    engine.say(text)
+    engine.runAndWait()
+    engine.stop()
+    
+if __name__ == "__main__":
+    speak("Voice Asistant Active")
+    while True:
+        r = sr.Recognizer()
+        with sr.Microphone() as source:
+            print("Voice Asistant is Listening")
+            try:
+                audio = r.listen(source,timeout=4)
+            except sr.exceptions.WaitTimeoutError:
+                speak("I am sorry I could not hear you")
+        try:
+            command = r.recognize_google(audio)
+            print(command)
+        except sr.UnknownValueError:
+            print("Voice Assistant could not understand audio")
+        except Exception:
+            speak("I am sorry I could not hear you")
