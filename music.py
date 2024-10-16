0e07c5100e12832d6f01ec0476e8967b1ca21c3f
notaarryan <aryanparmar2503@gmail.com>
2024-10-16 12:10:48 -0400

    hid api key

--- music.py
+++ music.py
@@ -1,5 +1,14 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client_id = os.getenv("client_id")
+client_secret = os.getenv("client_secret")
+
+print(client_id,client_secret)
 
 def getAlbums(a):
     results = sp.artist_albums(artistURI[a], album_type='album')
@@ -11,9 +20,6 @@ def getAlbums(a):
     for album in albums:
         print(album['name'])
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id="00dcbfe71cdd4c1d93787ac467149f79",
-                                               client_secret="2fa81312204c4f7591046db197cf7394",
-                                               redirect_uri="http://localhost:1234/",
-                                               scope="user-library-read"))
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id,client_secret,redirect_uri="http://localhost:1234/",scope="user-library-read"))
 
 artistURI = {"Karan Aujla" : "spotify:artist:6DARBhWbfcS9E4yJzcliqQ", "Travis Scott" : "spotify:artist:0Y5tJX1MQlPlqiwlOH1tJY","Drake":"spotify:artist:3TVXtAsR1Inumwj472S9r4"}
